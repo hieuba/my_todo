66631412ebd2ced770c7717f9d00e9ffc0c0cb55
Hieu Ba <121371793+hieuba@users.noreply.github.com>
2023-10-05 11:48:41 +0700

    sharedpre (#4)
    Co-authored-by: HIEU BA <hieuba@192.168.2.17>

---
+++ lib/services/storage_service.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: unused_field
+
+import 'package:my_todo/utils/constans.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class StorageService {
+  late final SharedPreferences _pref;
+
+  Future<StorageService> initStorage() async {
+    _pref = await SharedPreferences.getInstance();
+
+    return this;
+  }
+
+  Future<bool> setBool(String key, bool value) async {
+    return _pref.setBool(key, value);
+  }
+
+  Future<bool> setString(String key, String value) {
+    return _pref.setString(key, value);
+  }
+
+  bool getDeviceFirstOpen() {
+    return _pref.getBool(AppConstants.STORAGE_DEVICE_OPEN_FIRST_TIME) ?? false;
+  }
+
+  bool isLoggedIn() {
+    return _pref.getString(AppConstants.STORAGE_USER_TOKEN_KEY) == null
+        ? false
+        : true;
+  }
+}
